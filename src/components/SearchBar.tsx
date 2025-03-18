22ffce9a32d74803899741fb549775d2038f703c
Orlando Pedrazzoli <pedrazzoliorlando@gmail.com>
2025-03-18 21:11:17 +0000

    creating components

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image';
+
+const SearchBar = () => {
+  return (
+    <form className='flex ic justify-between gap-4 bg-gray-200 p-2 rounded-md flex-1'>
+      <input type='text' placeholder='Search' />
+      <button className='cursor-pointer'>
+        <Image src='/search.png' alt='' width={16} height={16} />
+      </button>
+    </form>
+  );
+};
+
+export default SearchBar;
