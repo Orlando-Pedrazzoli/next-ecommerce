298d6ccf1896d76abc71137cadf8ae92f8be00d3
Orlando Pedrazzoli <pedrazzoliorlando@gmail.com>
2025-03-19 12:38:02 +0000

    Cart Modal created

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,21 +10,28 @@ const Navbar = () => {
       {/*MOBILE*/}
       <div className='h-full flex items-center justify-between md:hidden'>
         <Link href='/'>
-          <div className='text-2xl tracking-wide'>ELITE SURFING</div>
+          <div className='text-2xl tracking-wide'>ES</div>
         </Link>
         <Menu />
       </div>
       {/*  BIGGER SCREENS */}
       <div className='hidden md:flex items-center justify-between gap-8 h-full'>
         {/* LEFT SIDE */}
-        <div className='w-1/3'>
+        <div className='w-1/3 xl:w-1/2 flex items-center gap-12 '>
           <Link href='/' className='flex items-center gap-3'>
             <Image src='/logo.png' alt='' width={24} height={24} />
-            <div className='text-2xl tracking-wide'>ELITE SURFING</div>
+            <div className='text-2xl tracking-wide'>ES</div>
           </Link>
+          <div className='hidden xl:flex gap-4'>
+            <Link href='/'>HomePage</Link>
+            <Link href='/'>Shop</Link>
+            <Link href='/'>Deals</Link>
+            <Link href='/'>About</Link>
+            <Link href='/'>Contact</Link>
+          </div>
         </div>
         {/* RIGHT SIDE */}
-        <div className='w-2/3 flex items-center justify-between gap-8'>
+        <div className='w-2/3 xl:w-1/2 flex items-center justify-between gap-8'>
           <SearchBar />
           <NavIcons />
         </div>
