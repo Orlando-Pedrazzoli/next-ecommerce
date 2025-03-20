c188d745c27f0970a9b978767d4cd7c28e2e9839
Orlando Pedrazzoli <pedrazzoliorlando@gmail.com>
2025-03-20 17:20:23 +0000

    updating components

--- src/components/Add.tsx
+++ src/components/Add.tsx
@@ -5,23 +5,48 @@ import { useState } from 'react';
 const Add = () => {
   const [quantity, setQuantity] = useState(1);
 
+  /* TEMPORARY */
+  const stock = 4;
+
+  const handleQuantity = (type: 'i' | 'd') => {
+    if (type === 'd' && quantity > 1) {
+      setQuantity(prev => prev - 1);
+    }
+    if (type === 'i' && quantity < stock) {
+      setQuantity(prev => prev + 1);
+    }
+  };
+
   return (
     <div className='flex flex-col gap-4'>
       <h4 className='font-medium'>Choose a Quantity</h4>
-      <div className=''>
-        <div className=''>
-          <button>+</button>
-          <button>-</button>
+      <div className='flex justify-between'>
+        <div className='flex items-center gap-4'>
+          <div className='bg-gray-200 py-2 px-4 rounded-3xl flex items-center justify-between w-32'>
+            <button
+              className='cursor-pointer text-xl'
+              onClick={() => handleQuantity('d')}
+            >
+              -
+            </button>
+            {quantity}
+            <button
+              className='cursor-pointer text-xl'
+              onClick={() => handleQuantity('i')}
+            >
+              +
+            </button>
+          </div>
+          <div className='text-xs'>
+            Only <span className='text-orange-500'>4 itens</span> left! <br />
+            {"Don't"}
+            miss it
+          </div>
         </div>
-        <div className='text-xs'>
-          Only <span className='text-orange-500'>4 itens</span> left! <br />
-          {"Don't"}
-          miss it
-        </div>
+        <button className='w-36 text-sm rounded-3xl ring-1 ring-lama text-lama py-2 px-4 hover:bg-lama hover:text-white disabled:cursor-not-allowed disabled:bg-pink-200 disabled:ring-0 disabled:text-white disabled:ring-none'>
+          Add to Cart
+        </button>
       </div>
-      <button className='w-36 text-sm rounded-3xl ring-1 ring-lama text-lama py-2 px-4 hover:bg-lama hover:text-white disabled:cursor-not-allowed disabled:bg-pink-200 disabled:ring-0 disabled:text-white disabled:ring-none'>
-        Add to Cart
-      </button>
     </div>
   );
 };
