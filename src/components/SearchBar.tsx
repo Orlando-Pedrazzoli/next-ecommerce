17bdd2fd7087d08df58d56196ae11fd79c6176b3
Orlando Pedrazzoli <pedrazzoliorlando@gmail.com>
2025-03-21 22:04:22 +0000

    Authentication

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -8,7 +8,6 @@ const SearchBar = () => {
 
   const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-
     const formData = new FormData(e.currentTarget);
     const name = formData.get('name') as string;
 
@@ -19,7 +18,7 @@ const SearchBar = () => {
 
   return (
     <form
-      className='flex ic justify-between gap-4 bg-gray-200 p-2 rounded-md flex-1'
+      className='flex items-center justify-between gap-4 bg-gray-100 p-2 rounded-md flex-1'
       onSubmit={handleSearch}
     >
       <input
